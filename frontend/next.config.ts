d6e0fee6927b4d70bb13915e062c59ebd17291da
Scott Jason <scottleejason@gmail.com>
2025-05-24 16:26:36 -0700

    update csp

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,33 +1,44 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
+const origin = isProd ? 'https://doculock.vercel.app' : 'http://localhost:3000';
+
+const ContentSecurityPolicy = `
+  frame-ancestors 'none';
+  default-src 'none';
+  script-src 'self' ${origin};
+  style-src 'self' ${origin};
+  img-src 'self' ${origin} data:;
+  font-src 'self' ${origin};
+  connect-src 'self' ${origin};
+`.replace(/\n/g, '');
 
 const securityHeaders = [
   {
-    key: "Cache-Control",
-    value: "no-store",
+    key: 'Cache-Control',
+    value: 'no-store',
   },
   {
-    key: "Content-Security-Policy",
-    value: "frame-ancestors 'none'",
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy,
   },
   {
-    key: "Strict-Transport-Security",
-    value: "max-age=63072000; includeSubDomains; preload",
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
   },
   {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
   },
   {
-    key: "X-Frame-Options",
-    value: "DENY",
+    key: 'X-Frame-Options',
+    value: 'DENY',
   },
 ];
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: '/(.*)',
         headers: securityHeaders,
       },
     ];
